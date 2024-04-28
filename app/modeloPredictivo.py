028963294596a32783715ad1e615c1937e12716f
Diego Salazar <diego.salazarc4@gmail.com>
2024-04-27 20:15:22 -0500

    updating of the DB, predictive model and tests

--- app/modeloPredictivo.py
+++ app/modeloPredictivo.py
@@ -7,9 +7,56 @@ import numpy as np
 df = pd.read_csv("SakuraStylishDB.csv", delimiter=";")
 print(df)
 
+
 df = df.drop(['Shirts', 'Trousers', 'Shoes'], axis=1)
 print(df)
 
 
-df['Sex Gender'] = df['Sex Gender'].map({'F': 0, 'M': 1})
- 
+df['Sex_Gender'] = df['Sex_Gender'].map({'F': 0, 'M': 1})
+
+
+X = df.drop('Type_Clothing_Event', axis=1)
+y = df['Type_Clothing_Event']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+categorical_cols = ['Age', 'Size', 'Weather', 'Time', 'Event']
+encoder = OneHotEncoder(drop='first')
+X_train_encoded = encoder.fit_transform(X_train[categorical_cols])
+X_test_encoded = encoder.transform(X_test[categorical_cols])
+
+ # Concatenar las variables codificadas con las características originales
+X_train_encoded = np.hstack((X_train_encoded.toarray(), X_train.drop(categorical_cols, axis=1)))
+X_test_encoded = np.hstack((X_test_encoded.toarray(), X_test.drop(categorical_cols, axis=1)))
+
+# Paso 4: Normalización de los datos
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train_encoded)
+X_test_scaled = scaler.transform(X_test_encoded)
+
+# Paso 5: Entrenamiento del modelo
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train_scaled, y_train)
+
+# Paso 6: Realización de una predicción con nuevos datos
+# Supongamos que tenemos un nuevo vector de datos para predecir la salida
+new_data = pd.DataFrame({'Age': ['Adult'], 'Size': ['L'], 'Weather': ['Cold'], 'Time': ['Night'], 'Sex_Gender': ['F'], 'Event': ['Marriage'], 'Satisfaction': [1] })
+
+# Convertir la columna 'Sex' a valores numéricos
+new_data['Sex_Gender'] = new_data['Sex_Gender'].map({'F': 0, 'M': 1})
+
+# Codificar la columna 'Embarked' utilizando el mismo encoder
+new_data_encoded = encoder.transform(new_data[['Age', 'Size', 'Weather', 'Time', 'Event']])
+
+# Concatenar las variables codificadas con las características originales
+new_data_encoded = np.hstack((new_data_encoded.toarray(), new_data.drop(['Age', 'Size', 'Weather', 'Time', 'Event'], axis=1)))
+
+# Normalizar el nuevo vector de datos utilizando el mismo escalador
+new_data_scaled = scaler.transform(new_data_encoded)
+
+# Predecir la salida del nuevo vector de datos
+prediction = model.predict(new_data_scaled)
+print("Predicción:", prediction)
+
+
+
