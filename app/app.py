2636696f058095c4df20479ce2d96c74ffb9dc76
Diego Salazar <diego.salazarc4@gmail.com>
2024-04-29 18:57:09 -0500

    updating

--- app/app.py
+++ app/app.py
@@ -1,17 +1,34 @@
-from flask import Flask, render_template, url_for
-#from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Flask, render_template, request, jsonify
+import joblib
 
 app = Flask(__name__)
+model = joblib.load('modeloPredictivo.pkl')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Crea las tablas de la base de datos cuando se acceda a la página de inicio
-    return render_template('index.html')
+    if request.method == 'POST':
+        try:
+            # Obtener los datos de entrada de la solicitud
+            data = request.json['data']
+
+            # Realizar la predicción utilizando el modelo cargado
+            prediction = model.predict([data])[0]
+
+            # Mapear el número de clase a la etiqueta correspondiente
+            target_names = ['Age', 'Size', 'Weather', 'Time', 'Sex_Gender', 'Event', 'Satisfaction']
+            predicted_class = target_names[prediction]
+
+            return jsonify({'prediction': predicted_class})
+        except Exception as e:
+            # Manejar cualquier error que ocurra durante la predicción
+            return jsonify({'error': str(e)}), 400
+    else:
+        # Renderizar la página de inicio
+        return render_template('index.html')
 
 @app.route('/carrito')
 def carrito():
-    # Crea las tablas de la base de datos cuando se acceda a la página de inicio
+    # Renderizar la página del carrito
     return render_template('carrito.html')
 
 if __name__ == "__main__":
